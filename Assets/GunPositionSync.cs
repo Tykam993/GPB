7f9239939a41c61942c2c11a45d620774175ea1a
Tyler Kaminsky <tylerkaminsky@gmail.com>
2017-03-15 17:55:27 -0400

    gun moves with camera

---
+++ Assets/GunPositionSync.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class GunPositionSync : NetworkBehaviour
+{
+    [SerializeField]
+    Transform cameraTransform;
+    //[SerializeField]
+    //Transform handMount;
+    [SerializeField]
+    Transform gunPivot;
+    [SerializeField]
+    float threshold = 10f;
+    [SerializeField]
+    float smoothing = 5f;
+
+    [SyncVar]
+    float pitch;
+    Vector3 lastOffset;
+    float lastSyncedPitch;
+
+    void Start()
+    {
+        if (isLocalPlayer)
+            gunPivot.parent = cameraTransform;
+        else
+            lastOffset = transform.position - transform.position;
+    }
+
+    void Update()
+    {
+        if (isLocalPlayer)
+        {
+            pitch = cameraTransform.localRotation.eulerAngles.x;
+            if (Mathf.Abs(lastSyncedPitch - pitch) >= threshold)
+            {
+                CmdUpdatePitch(pitch);
+                lastSyncedPitch = pitch;
+            }
+        }
+        else
+        {
+            Quaternion newRotation = Quaternion.Euler(pitch, 0f, 0f);
+
+            Vector3 currentOffset = transform.position - transform.position;
+            gunPivot.localPosition += currentOffset - lastOffset;
+            lastOffset = currentOffset;
+
+            gunPivot.localRotation = Quaternion.Lerp(gunPivot.localRotation,
+                newRotation, Time.deltaTime * smoothing);
+        }
+    }
+
+    [Command]
+    void CmdUpdatePitch(float newPitch)
+    {
+        pitch = newPitch;
+    }
+
+}
